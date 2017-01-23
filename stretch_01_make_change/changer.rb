ecd1954589f5545b75a8299cf7c1320e72da1272
Bence Kiss <kiss.bence.balint@gmail.com>
2017-01-23 18:59:22 -0500

    Made a lot of change from big money.

---
+++ stretch_01_make_change/changer.rb
@@ -0,0 +1,20 @@
+class Changer
+  def self.make_change(big)
+    change = []
+    while big > 0
+      if big % 25 == 0 && big >= 25
+        num = 25
+      elsif big % 10 == 0 && big >= 10
+        num = 10
+      elsif big % 5 == 0 && big >= 5
+        num = 5
+      else
+        num = 1
+      end
+      change << num
+      big -= num
+    end
+    change.sort!.reverse!
+  end
+
+end
