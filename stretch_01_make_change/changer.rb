5ff42b67db02a957f8e4bc2faafa6c0bc161540f
Bence Kiss <kiss.bence.balint@gmail.com>
2017-01-27 09:59:00 -0500

    taking out the divisibles

--- stretch_01_make_change/changer.rb
+++ stretch_01_make_change/changer.rb
@@ -2,11 +2,11 @@ class Changer
   def self.make_change(big)
     change = []
     while big > 0
-      if big % 25 == 0 && big >= 25
+      if big >= 25
         num = 25
-      elsif big % 10 == 0 && big >= 10
+      elsif big >= 10
         num = 10
-      elsif big % 5 == 0 && big >= 5
+      elsif big >= 5
         num = 5
       else
         num = 1
@@ -18,3 +18,5 @@ class Changer
   end
 
 end
+
+puts Changer.make_change(35)
